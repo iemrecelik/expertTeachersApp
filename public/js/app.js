4519766df26d9540de88536a695234d9293d428a
Emre ÇELİK <iemrecelik@hotmail.com>
2022-08-23 09:58:39 +0300

    app.js dosyasını kaydetme

--- public/js/app.js
+++ public/js/app.js
@@ -6787,7 +6787,7 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
       // let form = $('#' + this.formIDName)[0];
 
       /* let form = $('#' + this.$parent.$parent.formIDName)[0];
-       let data = new FormData(form); */
+        let data = new FormData(form); */
       var files = event.target.files;
       var data = new FormData();
 
@@ -8116,7 +8116,7 @@ var render = function render() {
     attrs: {
       "for": "exampleInputEmail1"
     }
-  }, [_vm._v("\n          " + _vm._s(_vm.$t("messages.categoryName")) + "\n        ")]), _vm._v(" "), _c("treeselect", {
+  }, [_vm._v("\r\n          " + _vm._s(_vm.$t("messages.categoryName")) + "\r\n        ")]), _vm._v(" "), _c("treeselect", {
     attrs: {
       name: "dc_cat_id",
       options: _vm.categoryList,
@@ -8261,7 +8261,7 @@ var render = function render() {
     attrs: {
       "for": "exampleInputEmail1"
     }
-  }, [_vm._v("\n          " + _vm._s(_vm.$t("messages.categoryName")) + "\n        ")]), _vm._v(" "), _c("treeselect", {
+  }, [_vm._v("\r\n          " + _vm._s(_vm.$t("messages.categoryName")) + "\r\n        ")]), _vm._v(" "), _c("treeselect", {
     attrs: {
       name: "dc_cat_id",
       options: _vm.categoryList,
@@ -8316,7 +8316,7 @@ var render = function render() {
     attrs: {
       id: "formModalLongTitle"
     }
-  }, [_vm._v("\n      " + _vm._s(_vm.titleName) + "\n    ")]), _vm._v(" "), _vm._m(0)]), _vm._v(" "), _c("div", {
+  }, [_vm._v("\r\n      " + _vm._s(_vm.titleName) + "\r\n    ")]), _vm._v(" "), _vm._m(0)]), _vm._v(" "), _c("div", {
     staticClass: "modal-body"
   }, [_vm._t("default")], 2), _vm._v(" "), _c("div", {
     staticClass: "modal-footer"
@@ -8328,7 +8328,7 @@ var render = function render() {
     on: {
       click: _vm.saveMethod
     }
-  }, [_vm._v("\n      " + _vm._s(_vm.saveBtnName) + "\n      "), _c("div", {
+  }, [_vm._v("\r\n      " + _vm._s(_vm.saveBtnName) + "\r\n      "), _c("div", {
     staticClass: "ld ld-ring ld-spin p-2"
   })]), _vm._v(" "), _c("button", {
     staticClass: "btn btn-secondary",
@@ -8336,7 +8336,7 @@ var render = function render() {
       type: "button",
       "data-dismiss": "modal"
     }
-  }, [_vm._v("\n      " + _vm._s(_vm.cancelBtnName) + "\n    ")])])]);
+  }, [_vm._v("\r\n      " + _vm._s(_vm.cancelBtnName) + "\r\n    ")])])]);
 };
 
 var staticRenderFns = [function () {
@@ -8395,7 +8395,7 @@ var render = function render() {
       "data-datas": "{\"formTitleName\": \"${formTitleName}\"}",
       "data-component": "".concat(_vm.formTitleName, "-create-component")
     }
-  }, [_vm._v("\n            " + _vm._s(_vm.$t("messages.add")) + "\n          ")])])])])]), _vm._v(" "), _c("div", {
+  }, [_vm._v("\r\n            " + _vm._s(_vm.$t("messages.add")) + "\r\n          ")])])])])]), _vm._v(" "), _c("div", {
     staticClass: "modal fade",
     attrs: {
       tabindex: "-1",
@@ -8492,7 +8492,7 @@ var render = function render() {
     attrs: {
       "for": "exampleInputEmail1"
     }
-  }, [_vm._v("\n\t\t\t\t\t\t" + _vm._s(_vm.$t("messages.categoryName")) + "\n\t\t\t\t\t")]), _vm._v(" "), _c("treeselect", {
+  }, [_vm._v("\r\n\t\t\t\t\t\t" + _vm._s(_vm.$t("messages.categoryName")) + "\r\n\t\t\t\t\t")]), _vm._v(" "), _c("treeselect", {
     attrs: {
       name: "dc_cat_id",
       options: _vm.categoryList,
@@ -8567,7 +8567,7 @@ var render = function render() {
         return _vm.addRelForm();
       }
     }
-  }, [_vm._v("\n\t\t\t\t\t\t\tİlgi Formu Ekle\n\t\t\t\t\t\t")])])]), _vm._v(" "), _c("p", [_c("button", {
+  }, [_vm._v("\r\n\t\t\t\t\t\t\tİlgi Formu Ekle\r\n\t\t\t\t\t\t")])])]), _vm._v(" "), _c("p", [_c("button", {
     staticClass: "btn btn-primary",
     attrs: {
       type: "button",
@@ -8576,7 +8576,7 @@ var render = function render() {
       "aria-expanded": "false",
       "aria-controls": "collapseExample"
     }
-  }, [_vm._v("\n\t\t\t\t\t\tGelişmiş Ekleme\n\t\t\t\t\t")])])], 2)]), _vm._v(" "), _c("button", {
+  }, [_vm._v("\r\n\t\t\t\t\t\tGelişmiş Ekleme\r\n\t\t\t\t\t")])])], 2)]), _vm._v(" "), _c("button", {
     staticClass: "btn btn-primary",
     attrs: {
       type: "submit"
@@ -8650,7 +8650,7 @@ var render = function render() {
     }
   }, [_vm._v("Gelen Evrak")])]), _vm._v(" "), _c("div", {
     staticClass: "invalid-feedback"
-  }, [_vm._v("\n\t\t\t\t\tLütfen evrağın durumunu seçiniz.\n\t\t\t\t")])])])]), _vm._v(" "), _c("div", {
+  }, [_vm._v("\r\n\t\t\t\t\tLütfen evrağın durumunu seçiniz.\r\n\t\t\t\t")])])])]), _vm._v(" "), _c("div", {
     staticClass: "row"
   }, [_c("div", {
     staticClass: "col-3"
@@ -8676,7 +8676,7 @@ var render = function render() {
     attrs: {
       id: "emailHelp"
     }
-  }, [_vm._v("\n\t\t\t\t\tudf formatındaki evrağı yükleyin.\n\t\t\t\t")])]), _vm._v(" "), _c("div", {
+  }, [_vm._v("\r\n\t\t\t\t\tudf formatındaki evrağı yükleyin.\r\n\t\t\t\t")])]), _vm._v(" "), _c("div", {
     staticClass: "form-group"
   }, [_c("label", {
     attrs: {
@@ -8696,7 +8696,7 @@ var render = function render() {
     attrs: {
       id: "emailHelp"
     }
-  }, [_vm._v("\n\t\t\t\t\t\t\tBuraya evrakın eklerini ekleyiniz.\n\t\t\t\t\t")])])]), _vm._v(" "), _c("div", {
+  }, [_vm._v("\r\n\t\t\t\t\t\t\tBuraya evrakın eklerini ekleyiniz.\r\n\t\t\t\t\t")])])]), _vm._v(" "), _c("div", {
     staticClass: "col-5"
   }, [_c("div", {
     staticClass: "form-group mb-1"
@@ -8739,7 +8739,7 @@ var render = function render() {
     attrs: {
       id: "emailHelp"
     }
-  }, [_vm._v("\n\t\t\t\t\tEvrağın gönderildiği tarih.\n\t\t\t\t")])]), _vm._v(" "), _c("div", {
+  }, [_vm._v("\r\n\t\t\t\t\tEvrağın gönderildiği tarih.\r\n\t\t\t\t")])]), _vm._v(" "), _c("div", {
     staticClass: "form-group"
   }, [_c("label", {
     attrs: {
@@ -8762,7 +8762,7 @@ var render = function render() {
     attrs: {
       id: "emailHelp"
     }
-  }, [_vm._v("\n\t\t\t\t\tEvrağın konusunu içerir.\n\t\t\t\t")])])]), _vm._v(" "), _c("div", {
+  }, [_vm._v("\r\n\t\t\t\t\tEvrağın konusunu içerir.\r\n\t\t\t\t")])])]), _vm._v(" "), _c("div", {
     staticClass: "col-4"
   }, [_c("div", {
     staticClass: "form-group"
@@ -8810,7 +8810,7 @@ var render = function render() {
     attrs: {
       id: "emailHelp"
     }
-  }, [_vm._v("\n\t\t\t\t\tEvrağın gönderildiği yer.\n\t\t\t\t")])])])])]);
+  }, [_vm._v("\r\n\t\t\t\t\tEvrağın gönderildiği yer.\r\n\t\t\t\t")])])])])]);
 };
 
 var staticRenderFns = [];
@@ -9001,7 +9001,7 @@ var render = function render() {
     }
   }, [_c("i", {
     staticClass: "fa fa-search"
-  }), _vm._v("\n            Ara\n          ")])])], 1), _vm._v(" "), _c("div", {
+  }), _vm._v("\r\n            Ara\r\n          ")])])], 1), _vm._v(" "), _c("div", {
     staticClass: "row mt-3"
   }, [_c("div", {
     staticClass: "col-md-10 offset-md-1"
@@ -9199,7 +9199,7 @@ var staticRenderFns = [function () {
     attrs: {
       type: "button"
     }
-  }, [_vm._v("\n                    EKLE\n                  ")])])])]);
+  }, [_vm._v("\r\n                    EKLE\r\n                  ")])])])]);
 }];
 render._withStripped = true;
 
@@ -9243,7 +9243,7 @@ var staticRenderFns = [function () {
     attrs: {
       type: "button"
     }
-  }, [_vm._v("\n          asdasdasd\n        ")])])])])]);
+  }, [_vm._v("\r\n          asdasdasd\r\n        ")])])])])]);
 }];
 render._withStripped = true;
 
@@ -9629,7 +9629,7 @@ var staticRenderFns = [function () {
       "aria-controls": "pills-home",
       "aria-selected": "true"
     }
-  }, [_vm._v("\n                    Eski Yönetmelik\n                  ")])]), _vm._v(" "), _c("li", {
+  }, [_vm._v("\r\n                    Eski Yönetmelik\r\n                  ")])]), _vm._v(" "), _c("li", {
     staticClass: "nav-item",
     attrs: {
       role: "presentation"
@@ -9645,7 +9645,7 @@ var staticRenderFns = [function () {
       "aria-controls": "pills-profile",
       "aria-selected": "false"
     }
-  }, [_vm._v("\n                    Yeni Yönetmelik\n                  ")])]), _vm._v(" "), _c("li", {
+  }, [_vm._v("\r\n                    Yeni Yönetmelik\r\n                  ")])]), _vm._v(" "), _c("li", {
     staticClass: "nav-item",
     attrs: {
       role: "presentation"
@@ -9661,7 +9661,7 @@ var staticRenderFns = [function () {
       "aria-controls": "pills-contact",
       "aria-selected": "false"
     }
-  }, [_vm._v("\n                    Bilgi Ekleme\n                  ")])])])]);
+  }, [_vm._v("\r\n                    Bilgi Ekleme\r\n                  ")])])])]);
 }];
 render._withStripped = true;
 
@@ -31709,7 +31709,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\n.upload-container {\n\tposition: relative;\n}\n.upload-container input {\n\t/* border: 1px solid #92b0b3;\n\tbackground: #f1f1f1;\n\toutline: 2px dashed #92b0b3;\n\toutline-offset: -10px;\n\tpadding: 60px 0px 60px 80px;\n\ttext-align: center !important;\n\twidth: 330px; */\n\n\tborder: 1px solid #92b0b3;\n\tbackground: #f1f1f1;\n\toutline: 2px dashed #92b0b3;\n\toutline-offset: -10px;\n\tpadding: 43px 0px 36px 38px;\n\ttext-align: center !important;\n\twidth: 100%;\n}\n.upload-container input:hover {\n\tbackground: #ddd;\n}   \n/* .upload-container:before {\n\tposition: absolute;\n\tbottom: 50px;\n\tleft: 245px;\n\tcontent: \" (or) Drag and Drop files here. \";\n\tcolor: #3f8188;\n\tfont-weight: 900;\n}  */\n.upload-btn {\n\tmargin-left: 300px;\n\tpadding: 7px 20px;\n}     \n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n.upload-container {\r\n\tposition: relative;\n}\n.upload-container input {\r\n\t/* border: 1px solid #92b0b3;\r\n\tbackground: #f1f1f1;\r\n\toutline: 2px dashed #92b0b3;\r\n\toutline-offset: -10px;\r\n\tpadding: 60px 0px 60px 80px;\r\n\ttext-align: center !important;\r\n\twidth: 330px; */\r\n\r\n\tborder: 1px solid #92b0b3;\r\n\tbackground: #f1f1f1;\r\n\toutline: 2px dashed #92b0b3;\r\n\toutline-offset: -10px;\r\n\tpadding: 43px 0px 36px 38px;\r\n\ttext-align: center !important;\r\n\twidth: 100%;\n}\n.upload-container input:hover {\r\n\tbackground: #ddd;\n}   \r\n/* .upload-container:before {\r\n\tposition: absolute;\r\n\tbottom: 50px;\r\n\tleft: 245px;\r\n\tcontent: \" (or) Drag and Drop files here. \";\r\n\tcolor: #3f8188;\r\n\tfont-weight: 900;\r\n}  */\n.upload-btn {\r\n\tmargin-left: 300px;\r\n\tpadding: 7px 20px;\n}     \r\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
@@ -31733,7 +31733,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\nimg[data-v-29be5aad] {\n  max-width: 100%;\n}\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\nimg[data-v-29be5aad] {\r\n  max-width: 100%;\n}\r\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
