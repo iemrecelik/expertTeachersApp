75fea719d0d15458895be50248b92730c5345386
Emre ÇELİK <iemrecelik@hotmail.com>
2022-09-23 17:23:04 +0300

    windwos 23.09.2022

--- resources/js/components/admin/document_mng/document/CreateComponent.vue
+++ resources/js/components/admin/document_mng/document/CreateComponent.vue
@@ -391,39 +391,5 @@ export default {
 </script>
 
 <style>
-.upload-container {
-	position: relative;
-}
-.upload-container input {
-	/* border: 1px solid #92b0b3;
-	background: #f1f1f1;
-	outline: 2px dashed #92b0b3;
-	outline-offset: -10px;
-	padding: 60px 0px 60px 80px;
-	text-align: center !important;
-	width: 330px; */
 
-	border: 1px solid #92b0b3;
-	background: #f1f1f1;
-	outline: 2px dashed #92b0b3;
-	outline-offset: -10px;
-	padding: 43px 0px 36px 38px;
-	text-align: center !important;
-	width: 100%;
-}
-.upload-container input:hover {
-	background: #ddd;
-}   
-/* .upload-container:before {
-	position: absolute;
-	bottom: 50px;
-	left: 245px;
-	content: " (or) Drag and Drop files here. ";
-	color: #3f8188;
-	font-weight: 900;
-}  */  
-.upload-btn {
-	margin-left: 300px;
-	padding: 7px 20px;
-}     
 </style>
